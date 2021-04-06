f7328884968522e2ca550a0af8aff1196cd6f911
Nicke Nicrad <nickenicrad@gmail.com>
2021-04-06 14:42:10 +0200

    first commit

--- routes/visit.routes.js
+++ routes/visit.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {createVisit, updataVisit, getAllVisits} = require('../controllers/visits.controllers');
+const authenticated = require('../middleware/auth.middleware');
 
 router.get('/', getAllVisits);
 
-router.post('/', createVisit);
+router.post('/', authenticated, createVisit);
 
-router.patch('/', updataVisit);
+router.patch('/:id', authenticated, updataVisit);
+
+module.exports = router;
